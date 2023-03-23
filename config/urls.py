82835aef6aa579646dba392cd7029bf312ffeff7
Gary M <gary.fullstackdev@gmail.com>
2023-03-23 09:10:23 -0400

    working model

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("pages.urls")),
     path("posts/", include("posts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
 
